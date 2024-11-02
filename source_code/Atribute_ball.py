e0bc3cd0a1f1c06499b1689459971ea2cdf669ad
HauiZ <hauiz15122004@gmail.com>
2024-11-02 15:44:51 +0700

    fix atribute

--- source_code/Atribute_ball.py
+++ source_code/Atribute_ball.py
@@ -12,13 +12,13 @@ class Atribute_ball():
         self.ball_x = ball_x
         self.ball_y = ball_y
         self.ball_position = np.array([ball_x,ball_y],dtype= np.float64)
-        self.Gravity = 0.05
+        self.Gravity = 0.01
         self.Atribute_list = [self.speed,self.paddle_speed,self.size_ball,self.paddle_size]
         self.color = random.choice(self.Atribute_list)
         self.element = random.choice(self.Atribute_list)
         self.atribute = str(self.element)
         self.ball_radius = 50
-        self.ball_velocity = np.array([0,0.5],dtype= np.float64)
+        self.ball_velocity = np.array([0,0.1],dtype= np.float64)
         self.ball = pg.draw.circle(window,self.color,self.ball_position,self.ball_radius)
         self.active = True
         self.hit = False
